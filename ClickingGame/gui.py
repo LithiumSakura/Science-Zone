63fa1cefcea9c9d4ae45b92848a12b5440f5ccc9
Lisa McKenzie <145104175+LithiumSakura@users.noreply.github.com>
2025-12-06 09:48:24 +0000

    Add files via upload

--- ClickingGame/gui.py
+++ ClickingGame/gui.py
@@ -3,11 +3,10 @@ from pygame.locals import *
 
 
 class TextInput(pygame.Surface):
-
 	def __init__(self, pos, font_size, size, text="", margin=0, text_color=(200,200,200), background_color=(10,10,10)):
 		pygame.Surface.__init__(self, size)
 		
-		self.speed = 2 # 
+		self.speed = 2
 		self.text = text
 		self.active = False
 		self.pos = pos
@@ -16,7 +15,6 @@ class TextInput(pygame.Surface):
 		self.text_color = text_color
 		self.background_color = background_color
 		self.margin = margin
-		
 		self.active_bg_color = (min(self.background_color[0]+20,255), min(self.background_color[1]+20,255), min(self.background_color[2]+20,255))
 		
 	def onclick(self):
@@ -26,11 +24,10 @@ class TextInput(pygame.Surface):
 		return (pos[0] >= self.pos[0] and\
 		 pos[1] >= self.pos[1] and\
 		 pos[0] <= self.pos[0]+self.size[0] and\
-		 pos[1] <= self.pos[1]+self.size[1])	
+		 pos[1] <= self.pos[1]+self.size[1])
 	
 	def blit_on(self, surface):
-		
-		if self.active:	
+		if self.active:
 			self.fill(self.active_bg_color)
 		else:
 			self.fill(self.background_color)
@@ -44,19 +41,15 @@ class TextInput(pygame.Surface):
 
 
 class AnimatedSprite(pygame.Surface):
-
 	def __init__(self, pos, size):
 		pygame.Surface.__init__(self, size)
-		
 		self.frames = []
 		self.speed = 6
 		self.frame_counter = 0
 		self.background_color = (0,0,0)
 		self.pos = pos
 		
-	
 	def blit_on(self, surface):
-		
 		self.fill(self.background_color)
 		self.blit(self.frames[self.frame_counter], (0,0))
 		surface.blit(self, self.pos)
@@ -69,46 +62,36 @@ class AnimatedSprite(pygame.Surface):
 
 
 class Caption(pygame.Surface):
-	
 	def __init__(self, pos, font_size, size, text, margin=0, text_color=(100,100,100), background_color=(0,0,0)):
 		pygame.Surface.__init__(self, size)
-		
 		self.text = text
 		self.pos = pos
 		self.size = size
 		self.font_size = font_size
 		self.text_color = text_color
 		self.background_color = background_color
-		
 		self.margin = margin
 	
 	def blit_on(self, surface):
-		
 		self.fill(self.background_color)
 		font = pygame.font.Font(None, self.font_size)
 		text = font.render(self.text, 1, self.text_color)
 		self.blit(text, (self.margin,self.margin))
 		surface.blit(self, self.pos)
-		
+
 
 class Button(pygame.Surface):
-	
 	def __init__(self, pos, size, caption="", color=(100,100,100), text_color=(0,0,0)):
 		pygame.Surface.__init__(self, size)
-	
 		self.onclick = None
-	
 		self.pressed = False
 		self.hover = False
-		
 		self.caption = caption
 		self.pos = pos
 		self.size = size
 		self.color = color
 		self.text_color = text_color
-		
 		self.margin = 10
-		
 		self.pressed_color = (min(self.color[0]+100,255),min(self.color[1]+100,255),min(self.color[2]+100,255))
 		self.hover_color = (min(self.color[0]+50,255),min(self.color[1]+50,255),min(self.color[2]+50,255))
 	
@@ -118,22 +101,20 @@ class Button(pygame.Surface):
 			self.fill(self.hover_color)
 		if self.pressed:
 			self.fill(self.pressed_color)
-			
+
 		font = pygame.font.Font(None, 16)
 		text = font.render(self.caption, 1, self.text_color)
 		self.blit(text, (self.margin,self.margin))
 		surface.blit(self, self.pos)
-		
 	
 	def contains(self, pos):
 		return (pos[0] >= self.pos[0] and\
 		 pos[1] >= self.pos[1] and\
 		 pos[0] <= self.pos[0]+self.size[0] and\
 		 pos[1] <= self.pos[1]+self.size[1])
-	
+
 
 class GUI:
-	
 	def __init__(self, size):
 		pygame.init()
 		self.screen = pygame.display.set_mode(size)
@@ -143,7 +124,7 @@ class GUI:
 		self.text_inputs = []
 		self.background_image = None
 		self.running = True
-		
+
 	def start(self):
 		self.preloop()
 		clock = pygame.time.Clock()
@@ -177,25 +158,18 @@ class GUI:
 						if text_input.active:
 							text_input.key_pressed(event.key)
 				self.react(event)
-			
 			self.inloop()
-			
 			self.screen.fill((0,0,0))
 			if not self.background_image == None:
 				self.screen.blit(self.background_image, (0,0))
-			
 			for button in self.buttons:
 				button.blit_on(self.screen)
-				
 			for caption in self.captions:
 				caption.blit_on(self.screen)
-				
 			for sprite in self.sprites:
 				sprite.blit_on(self.screen)
-				
 			for text_input in self.text_inputs:
 				text_input.blit_on(self.screen)
-				
 			pygame.display.flip()
 			clock.tick(60)
 
@@ -206,13 +180,13 @@ class GUI:
 
 	def preloop(self):
 		pass
-		
+
 	def inloop(self):
-		pass	
-		
+		pass
+
 	def preexit(self):
 		pass
-	
+
 	def react(self, event):
 		pass
 
--- ClickingGame/main.py
+++ ClickingGame/main.py
@@ -1,12 +1,10 @@
 import gui, time, random, pygame
 
 class RunningButton(gui.Button):
-
 	size = (50, 50)
 
 	def __init__(self, window_size):
 		gui.Button.__init__(self, self.random_pos(window_size), self.size, "", (255, 0, 0))
-		
 		self.pressed_color = (0, 0, 255)
 		self.hover_color = (0, 0, 255)
 		self.window_size = window_size
@@ -19,20 +17,18 @@ class RunningButton(gui.Button):
 		pos_y = random.randint(self.size[1], size_y - self.size[1])
 		return (pos_x, pos_y)
 		
+		
 class CGUI(gui.GUI):
-
 	size = (500, 650)
 	
 	def __init__(self):
 		gui.GUI.__init__(self, self.size)
 		pygame.display.set_caption("clicking game")
 		pygame.mouse.set_visible(True)
-		
 		self.background_image = pygame.image.load("bg.png")
 		self.game_is_over = False
-		self.time_limit = 10. # in seconds
+		self.time_limit = 10.
 		self.reset_game()
-		
 		
 	def update_time_limit(self):
 		self.time_limit = 10.
@@ -42,7 +38,6 @@ class CGUI(gui.GUI):
 	def update_level(self):
 		self.level = 1 + self.score // 20000
 		
-	
 	def reset_game(self):
 		self.rb = RunningButton((500, 400))
 		self.rb.onclick = self.rb_clicked
@@ -61,12 +56,9 @@ class CGUI(gui.GUI):
 		self.score_caption = gui.Caption((10, 600), 50, (self.size[0], 50), str(self.score), 0, (255,255,255), (10,10,30))
 		self.captions.append(self.score_caption)
 		
-		
-		
 		self.last_clicktime = time.time()
 		self.update_time_limit()
 		
-	
 	def rb_clicked(self):
 		if not self.game_is_over:
 			self.rb.pos = self.rb.random_pos()
@@ -74,8 +66,6 @@ class CGUI(gui.GUI):
 			self.score += int(self.time_left * 100) * self.level
 		else:
 			pass
-			#self.game_is_over = False
-			#self.reset_game()
 		
 	def game_over(self):
 		self.time_left_caption.text = "GAME OVER"
@@ -100,7 +90,6 @@ class CGUI(gui.GUI):
 			self.rb.color = (red, green, blue)
 		self.update_level()
 		self.update_time_limit()
-		
 		
 		
 if __name__ == "__main__":
